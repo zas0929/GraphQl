cbaa907a134b1ab157310907e9b10c9c41559a57
Roman Lisovskiy <rolisovskiy@mail.ru>
2018-05-08 16:52:18 +0300

    Minimal React Webpack application

--- minimal-react-boilerplate/src/index.js
+++ minimal-react-boilerplate/src/index.js
@@ -1,1 +1,11 @@
-console.log('My Minimal React Webpack Babel Setup');
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const title = 'My Minimal React Webpack';
+
+ReactDOM.render(
+  <div>{title}</div>,
+  document.getElementById('app')
+);
+
+module.hot.accept(); //Comment it and browser will reload the page
