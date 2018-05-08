a4b31579d9b06cdf4be4bfeb14573897ad087b64
Roman Lisovskiy <rolisovskiy@mail.ru>
2018-05-08 15:25:13 +0300

    Add/remove stars with mutations

--- index.js
+++ index.js
@@ -14,17 +14,88 @@ const client = new ApolloClient({
   },
 });
 
-const GET_ORGANIZATION = gql`
-  {
-    organization(login: "the-road-to-learn-react") {
+// const GET_ORGANIZATION = gql`
+//   {
+//     organization(login: "the-road-to-learn-react") {
+//       name
+//       url
+//     }
+//   }
+// `;
+//
+// client
+//   .query({
+//     query: GET_ORGANIZATION,
+//   })
+//   .then(console.log);
+
+
+const GET_REPOSITORIES_OF_ORGANIZATION = gql`
+  query($organization: String!) {
+    organization(login: $organization) {
       name
       url
+      repositories(first: 5, orderBy: {field: STARGAZERS, direction: DESC}) {
+        edges {
+          node {
+            id
+            name
+            url
+            stargazers {
+              totalCount
+            }
+          }
+        }
+      }
+    }
+  }
+`;
+
+const ADD_STAR = gql`
+  mutation AddStar($repositoryId: ID!) {
+    addStar(input: { starrableId: $repositoryId }) {
+      starrable {
+        id
+        viewerHasStarred
+      }
+    }
+  }
+`;
+
+const REMOVE_STAR = gql`
+  mutation RemoveStar($repositoryId: ID!) {
+    removeStar(input: { starrableId: $repositoryId }) {
+      starrable {
+        id
+        viewerHasStarred
+      }
     }
   }
 `;
 
 client
   .query({
-    query: GET_ORGANIZATION,
+    query: GET_REPOSITORIES_OF_ORGANIZATION,
+    variables: {
+      organization: 'the-road-to-learn-react',
+    },
+  })
+  .then(console.log);
+
+// client
+//   .mutate({
+//     mutation: ADD_STAR,
+//     variables: {
+//       repositoryId: 'MDEwOlJlcG9zaXRvcnk2MzM1MjkwNw==',
+//     },
+//   })
+//   .then(console.log);
+
+client
+  .mutate({
+    mutation: REMOVE_STAR,
+    variables: {
+      repositoryId: 'MDEwOlJlcG9zaXRvcnk2MzM1MjkwNw==',
+    },
   })
   .then(console.log);
