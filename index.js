eebe1a464b3f263cebdcdf7da543ebfc66bf3c30
Roman Lisovskiy <rolisovskiy@mail.ru>
2018-05-08 11:29:22 +0300

    The very first graphQL query

---
+++ index.js
@@ -0,0 +1,30 @@
+require('dotenv').config()
+
+import 'cross-fetch/polyfill';
+import ApolloClient, { gql } from 'apollo-boost';
+
+const client = new ApolloClient({
+  uri: 'https://api.github.com/graphql',
+  request: operation => {
+    operation.setContext({
+      headers: {
+        authorization: `Bearer ${process.env.GITHUB_PERSONAL_ACCESS_TOKEN}`,
+      },
+    });
+  },
+});
+
+const GET_ORGANIZATION = gql`
+  {
+    organization(login: "the-road-to-learn-react") {
+      name
+      url
+    }
+  }
+`;
+
+client
+  .query({
+    query: GET_ORGANIZATION,
+  })
+  .then(console.log);
